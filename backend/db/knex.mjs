46ac7ab3276094c805e3d4134ad6042540e72b74
qahar <nurqahar.hq@gmail.com>
2026-07-27 09:30:42 +0700

    fix: update gitignore

--- backend/db/knex.mjs
+++
@@ -1,9 +0,0 @@
-//const knex = require("knex");
-//const knexfile = require("../knexfile.js");
-import knex from "knex";
-import knexfile from "../knexfile.js";
-
-const env = process.env.NODE_ENV || "development";
-const config = knexfile[env];
-
-export default knex(config);
